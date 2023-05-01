4886e32b0683e6344dcfc39311336273b20ab669
harsh savani <131761988+harshsavani69@users.noreply.github.com>
2023-05-01 15:07:53 +0530

    Add files via upload

---
+++ MANAGING INPUT OUTPUT 2 .C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+   int a,b;
+   clrscr();
+   printf("enter value a=");
+   scanf("%d",&b);
+   printf("enter value b=");
+   scanf("%d",&a);
+   printf("additon of b=%d",b);
+   printf("additon of a=%d",a);
+   getch();
+}
